e897479008b271c994513b23c55ed882ad43e48a
James Alexander <Jaymealexander.12@gmail.com>
2022-11-01 19:05:59 -0500

    added 4 products in db seeds and 1 product in console

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+product = Product.new(:name => "blender", :price => 55, :image_url => "https://blendjet.com/products/blendjet", :description => "It really does just blend everything up for you, it's that good" )
+product.save
+product = Product.new(:name => "laptop", :price => 950, :image_url => "https://www.notebookcheck.net/NVIDIA-GeForce-GTX-950.150992.0.html", :description => "A good laptop for the price. Not the hardest hitting but there when you need it." )
+product.save
+product = Product.new(:name => "couch", :price => 250, :image_url => "https://www.target.com/p/willow-sofa-lifestyle-solutions/", :description => "You'll never want to leave the couch again after buying this super comfortable sectional" )
+product.save
+product = Product.new(:name => "yoga ball", :price => 20, :image_url => "https://www.amazon.com/REEHUT-Anti-Burst-Exercise-Balance-Workout", :description => "So you started to get back into yoga, you're gonna need one of these eventually, so just buy it")
+product.save
