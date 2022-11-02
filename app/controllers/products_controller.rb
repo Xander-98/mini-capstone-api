b106a2a0a216c4a6f5afc60fe268b86a7d4dbdd2
James Alexander <Jaymealexander.12@gmail.com>
2022-11-02 18:58:21 -0500

    modified controller and routes for restful index and show actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,14 @@
 class ProductsController < ApplicationController
+
+def show
+    product = Product.find_by(id: params["id"])
+    render json: product.as_json
 end
+
+def index
+    products = Product.all
+    render json: products.as_json
+end
+
+end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-product = Product.new(:name => "blender", :price => 55, :image_url => "https://blendjet.com/products/blendjet", :description => "It really does just blend everything up for you, it's that good" )
-product.save
-product = Product.new(:name => "laptop", :price => 950, :image_url => "https://www.notebookcheck.net/NVIDIA-GeForce-GTX-950.150992.0.html", :description => "A good laptop for the price. Not the hardest hitting but there when you need it." )
-product.save
-product = Product.new(:name => "couch", :price => 250, :image_url => "https://www.target.com/p/willow-sofa-lifestyle-solutions/", :description => "You'll never want to leave the couch again after buying this super comfortable sectional" )
-product.save
-product = Product.new(:name => "yoga ball", :price => 20, :image_url => "https://www.amazon.com/REEHUT-Anti-Burst-Exercise-Balance-Workout", :description => "So you started to get back into yoga, you're gonna need one of these eventually, so just buy it")
-product.save
+Product.create!([
+  {name: "blender", price: 55, image_url: "https://blendjet.com/products/blendjet", description: "It really does just blend everything up for you, it's that good"},
+  {name: "laptop", price: 950, image_url: "https://www.notebookcheck.net/NVIDIA-GeForce-GTX-950.150992.0.html", description: "A good laptop for the price. Not the hardest hitting but there when you need it."},
+  {name: "couch", price: 250, image_url: "https://www.target.com/p/willow-sofa-lifestyle-solutions/", description: "You'll never want to leave the couch again after buying this super comfortable sectional"},
+  {name: "yoga ball", price: 20, image_url: "https://www.amazon.com/REEHUT-Anti-Burst-Exercise-Balance-Workout", description: "So you started to get back into yoga, you're gonna need one of these eventually, so just buy it"},
+  {name: "console", price: 1, image_url: "https://www.yourterminal.net", description: "literally your terminal on your computer, im selling to you"},
+  {name: "Medium sized gem", price: 9999, image_url: "https://www.theworldsmostmediumsizeddiamond.com", description: "Not the worlds biggest diamond, but not the smallest, somewhere in between"},
+  {name: "air pro ", price: 1000, image_url: "N/A", description: "Like the stuff you breath, but better"}
+])
