efe17a1cc2e5351286a3553b4214b6f782110da5
James Alexander <Jaymealexander.12@gmail.com>
2022-11-02 21:47:04 -0500

    added a frontend to see the results of our backend requests, and added real data to image_url(s)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,5 +10,35 @@ def index
     render json: products.as_json
 end
 
+def create
+    product = Product.new(
+    name: params[:name],
+    price: params[:price],
+    image_url: params[:image_url],
+    description: params[:description]
+    )
+    product.save
+    render json: product.as_json
+end
+
+def update
+    product = Product.find_by(id: params["id"])
+    
+    product.name = params[:name] || product.name
+    product.price= params[:price] || product.price
+    product.image_url= params[:image_url] || product.image_url
+    product.description= params[:description] || product.description
+
+    product.save
+    render json: product.as_json
+end
+
+def destroy
+    product_id = params[:id]
+    product = Product.find(product_id)
+    product.destroy
+    render json: {message: "Product has been deleted!"}
+end
+
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get "/products/:id" => "products#show"
   get "/products" => "products#index"
+  post "/products" => "products#create"
+  patch "/products/:id" => "products#update"
+  delete "/products/:id" => "products#destroy"
 end
