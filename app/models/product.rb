f8b777726b7ed0857354a626d9e0d8ed4c7ecaf6
James Alexander <Jaymealexander.12@gmail.com>
2022-11-03 19:07:00 -0500

    added model methods : "is_discounted","tax","total",and "friendly_created_at" in models file products.rb, and implemented them into the products_controller file, and showed results in request.hhtp file

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,20 @@
 class Product < ApplicationRecord
+
+    def friendly_created_at
+        created_at.strftime("%B %e, %Y")
+    end
+
+    def is_discounted?
+            price < 10
+    end
+    
+    def tax
+        tax = price * 0.09
+        tax
+    end
+
+    def total
+        total = tax + price
+        total
+    end
 end
