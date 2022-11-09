47a667e22d01794959f53090c4efa3d24dfa6087
James Alexander <Jaymealexander.12@gmail.com>
2022-11-08 21:11:18 -0600

    added image model with uniqueness validator

---
+++ app/models/image.rb
@@ -0,0 +1,5 @@
+class Image < ApplicationRecord
+
+    validates :url, uniqueness: true
+    
+end
--- db/migrate/20221109030417_update_images_to_products.rb
+++
@@ -1,7 +0,0 @@
-class UpdateImagesToProducts < ActiveRecord::Migration[7.0]
-  
-  def change
-    remove_column :products, :image_url, :string
-  end
-
-end
