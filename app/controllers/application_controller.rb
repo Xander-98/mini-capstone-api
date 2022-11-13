7ff3143d433d45a58c0e5cc1fc27c4d762fc81b5
James Alexander <Jaymealexander.12@gmail.com>
2022-11-13 14:06:32 -0600

    adds order indeex/show jb views, and replaces variables with templates in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,12 @@ class ApplicationController < ActionController::API
           end
         end
       end
+
+      def authenticate_admin
+        unless current_user && current_user.admin
+          render json: {}, status: :unauthorized
+        end
+      end
     
       def authenticate_user
         unless current_user
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,28 +1,38 @@
 class OrdersController < ApplicationController
-
+before_action :authenticate_admin
+before_action :authenticate_user
     def create
-        order = Order.new(
-            user_id: params[:user_id],
+        product = Product.find(params[:product_id])
+        subtotal = params[:quantity] * product.price
+        tax = subtotal * 0.09
+        total = subtotal + tax
+        @order = Order.new(
+            user_id: current_user.id,
             product_id: params[:product_id],
             quantity: params[:quantity],
-            subtotal: ,
-            tax: subtotal * 0.06,
-            total: subtotal + tax
-        )
-       if order.save
-        render json: order.as_json
+            subtotal: subtotal,
+            tax: tax,
+            total: total      
+ )
+       if @order.save
+        render template: "orders#show"
        else
         render json: {message: orders.errors.full_messages}, status: 422
     end
 
     def show
-        orders = Order.find(params["id"])
-        render json: orders.as_json
+        @order = Order.find(params["id"])
+        render template: "orders#show"
     end
 
     def index
-        order = Order.all 
-        render json: order.as_json
+        if current_user
+        @orders = current_user.orders
+        render template: "orders#index"
+        else
+            render json: [], status: :unauthorized
+        end
     end
-
 end
+end
+
